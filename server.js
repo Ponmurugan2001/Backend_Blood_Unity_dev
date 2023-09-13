3bcfa5b5327918333fa9152f27ea1152c0f4f14f
Ponmurugan2001 <20d228@psgtech.ac.in>
2023-09-13 21:27:46 +0530

    included cors

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const inventoryRouter=require("./routes/inventoryRoutes")
 const analyticsRouter= require("./routes/analyticsRoutes")
 const donarProfile=require("./routes/donorProfile")
 const recipientProfile=require("./routes/recipientProfile")
+const cors = require('cors');
+
+
 
 const app= express();
 require('dotenv').config();
 const dbconfig =require("./config/dbConfig")
 app.use(express.json())
+
+  
+  app.use(cors());  
 
 // http://localhost:9000/api/user/register
 app.use('/api/user',userRouter)
